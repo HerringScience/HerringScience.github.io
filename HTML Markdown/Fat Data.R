310a5cd7f68b42eac4831dc0ab07172edfd108fc
HerringScience <herringsciencetech@outlook.com>
2023-03-23 10:56:53 -0300

    Fat Data Update, Fixed Plans Print Format

--- HTML Markdown/Fat Data.R
+++ HTML Markdown/Fat Data.R
@@ -34,5 +34,20 @@ Com6 = read_csv("Comeaus 2022.csv")
 Scotia = read_csv("Scotia 2017-2018.csv")
 Unkn = read_csv("Unknown 2007-2013.csv")
 
-#Standardize Data
-t
+#Connors QC / structuring
+Con = Con %>% dplyr::select(Date = DATE, Vessel = BOAT, Ground = AREA, 
+                            Size4.5 = Size_4.5, Size4.5_5, Size5_6, Size6_7, Size7_8, 
+                            Size8_9, Size9_10, Size10_11, Size11_12, Size12 = Size_12, Month) %>%
+  mutate(Year = substr(Date,1,4))
+Con = pivot_longer(data=Con, cols=starts_with("Size"), names_to="Size", names_prefix = "Size", values_to="Fat") %>%
+  filter(Fat < 40)
+ggplot(data=Con, aes(x=Date, y=Fat, colour=Year)) +
+  geom_boxplot() +
+  labs(y="Fat Percent (%)")
+
+#Scotia QC / structuring
+Scotia = Scotia %>% mutate(Average = rowMeans(dplyr::select(., "#1":"#8"))) %>%
+  dplyr::select(Ground = Location, Vessel, Type, Filets = "No. Filets per kg", 
+                                  Size = "Size (cm)", Date_Time = Recorded, Average)
+Scotia %>% date(Date_Time)
+  
