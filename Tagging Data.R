b6c61d70e20bafc55c79a51e8f03e5a8077aee69
HerringScience <herringsciencetech@outlook.com>
2023-09-21 15:06:09 -0300

    2023 09 21

--- Tagging Data.R
+++ Tagging Data.R
@@ -6,11 +6,11 @@ library(tidyverse)
 library(measurements)
 
 # Set all tagging log data here
-Tag_Num = c(578551:578800) #Can add any tag entries, including breaks/gaps in the sequences
-Date = "2023-09-04" #"YYYY-MM-DD"
-Lat = "43 14 71" #Degree-Min-Sec format from the boat but only the numbers written with spaces (e.g. "44 16 23")
-Lon = "66 21 59"
-Vessel = "" #As written unless changed in script below: "Lady Melissa", "Sealife II", "Tasha Marie", "Lady Janice", "Morning Star"
+Tag_Num = c(528712:528847) #Can add any tag entries, including breaks/gaps in the sequences
+Date = "2023-09-12" #"YYYY-MM-DD"
+Lat = "43 19 09" #Degree-Min-Sec format from the boat but only the numbers written with spaces (e.g. "44 16 23")
+Lon = "66 09 42"
+Vessel = "Lady Melissa" #As written unless changed in script below: "Lady Melissa", "Sealife II", "Tasha Marie", "Lady Janice", "Morning Star"
 Survey = NA #Survey number for Scots Bay or German Bank when tags were applied, otherwise "NA"
 CTD = NA #Add the CTD id only if a TAGGER completed a cast (not the cast by the HSC tech)
 
