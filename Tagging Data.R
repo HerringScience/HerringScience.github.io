b6579f6f766379b5cc3048f7b1020dd5b65548a5
HerringScience <herringsciencetech@outlook.com>
2023-10-27 16:20:07 -0300

    2023-10-27
    Inputting Tags
    Correcting Plankton Jars
    Correcting SB11 and GB6 results

--- Tagging Data.R
+++ Tagging Data.R
@@ -6,11 +6,11 @@ library(tidyverse)
 library(measurements)
 
 # Set all tagging log data here
-Tag_Num = c(474601:474750) #Can add any tag entries, including breaks/gaps in the sequences
-Date = "2023-10-02" #"YYYY-MM-DD"
-Lat = "44 14 32" #Degree-Min-Sec format from the boat but only the numbers written with spaces (e.g. "44 16 23")
-Lon = "66 26 28"
-Vessel = "Morning Star" #As written unless changed in script below: "Lady Melissa", "Sealife II", "Tasha Marie", "Lady Janice", "Morning Star"
+Tag_Num = c(520001:520050) #Can add any tag entries, including breaks/gaps in the sequences
+Date = "2023-10-15" #"YYYY-MM-DD"
+Lat = "44 16 11" #Degree-Min-Sec format from the boat but only the numbers written with spaces (e.g. "44 16 23")
+Lon = "67 00 76"
+Vessel = "Sealife II" #As written unless changed in script below: "Lady Melissa", "Sealife II", "Tasha Marie", "Lady Janice", "Morning Star"
 Survey = NA #Survey number for Scots Bay or German Bank when tags were applied, otherwise "NA"
 CTD = NA #Add the CTD id only if a TAGGER completed a cast (not the cast by the HSC tech)
 
