835beb21c4dd943308a8efc935ecf5dbc6d32689
HerringScience <herringsciencetech@outlook.com>
2024-01-03 12:00:52 -0400

    2023 01 03
    Sunset Script

--- Tracey's Folder/Sunset Script.R
+++ Tracey's Folder/Sunset Script.R
@@ -47,6 +47,7 @@ date = "2024-01-03"
 #SunsetStart <- read_csv(paste0("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Tracey's Folder/surveyFactorsAll_Tracey with SSB data.csv"))
 SunsetStart <- read_csv(paste0("C:/Users/herri/Desktop/Tracey Local SSB Spreadsheet.csv"))
 setwd("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Tracey's Folder")
+SurveyData <- read_csv(paste0("C:/Users/herri/Desktop/Survey Data copy.csv"))
 
 #Lat and Lon Converter
 Lon = conv_unit(Lon,"deg_min_sec","dec_deg")
@@ -58,6 +59,8 @@ Lon = -1*Lon
 #Calendar date Converter
 date = as.Date(as.POSIXct(date))
 julian = yday(date)
+Year = format(date, "%Y")
+
 
 #Data Table of Sunset times - Current Data
 Sunset <- suncalc::getSunlightTimes(date = date, lat = Lat, lon = Lon, keep = c("sunsetStart"), tz = "America/Halifax")
@@ -70,8 +73,8 @@ SunsetStartOnly <- Sunset$sunsetStart
 SunsetStartOnly <- as.POSIXct(SunsetStartOnly)
 
 #Bind Data
-
 SunsetStart <- full_join(SunsetStart, (data.frame(Survey_Date = date,
+                                                 Year = as.double(Year),
                                                  sunsetStart = as.character(SunsetStartOnly),
                                                  Comments = "NA")))
 SunsetStart <- SunsetStart %>% mutate(id = row_number())
