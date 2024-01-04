0936412c5a6a777fe6f228ecc98afd7428477b28
HerringScience <herringsciencetech@outlook.com>
2024-01-04 09:01:36 -0400

    2023 01 03
    Sunset script update

--- Tracey's Folder/Sunset Script.R
+++ Tracey's Folder/Sunset Script.R
@@ -38,17 +38,30 @@ rm(list = ls())
 
 
 #Set information here
-Lat = "45 03 54" #Degree-Min-Sec format from the boat but only the numbers written with spaces (e.g. "44 16 23")
-Lon = "65 14 93"
 date = "2024-01-03"
+Ground="SI" #SB or GB or SI
+Survey.No="7"
+Survey_Type = "Structured" # Structured or Adhoc
+Counted = "Yes" #Biomass counted to the total SSB number. Surveys should be minimum 10 days aprt, largest biomass number counted.
+Comments = "NA" #Input any comments about the survey here. Example- The vessel Morning Star 
+
 
 ### Code below ###
 
+#If statements for ground coordinates
+
+if(Ground =="SB"){Lat = "45 03 00"
+                  Lon = "65 13 80"}
+
+if(Ground == "GB"| Ground == "SI") {Lat = "43 33 89" 
+                                    Lon = "66 21 48"}
+
 #SunsetStart <- read_csv(paste0("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Tracey's Folder/surveyFactorsAll_Tracey with SSB data.csv"))
-SunsetStart <- read_csv(paste0("C:/Users/herri/Desktop/Tracey Local SSB Spreadsheet.csv"))
+SSB <- read_csv(paste0("C:/Users/herri/Desktop/Tracey Local SSB Spreadsheet.csv"))
 setwd("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Tracey's Folder")
 SurveyData <- read_csv(paste0("C:/Users/herri/Desktop/Survey Data copy.csv"))
-
+  SurveyData <- SurveyData %>% slice_tail()
+  
 #Lat and Lon Converter
 Lon = conv_unit(Lon,"deg_min_sec","dec_deg")
 Lon = as.numeric(Lon)
@@ -65,16 +78,35 @@ Year = format(date, "%Y")
 #Data Table of Sunset times - Current Data
 Sunset <- suncalc::getSunlightTimes(date = date, lat = Lat, lon = Lon, keep = c("sunsetStart"), tz = "America/Halifax")
 Sunset <- as.data.frame(Sunset)
-SunsetStart <- as.data.frame(SunsetStart)
+SSB <- as.data.frame(SSB)
 Sunset[] <- data.frame(lapply(Sunset, as.character), stringsAsFactors = FALSE)
 Sunset$date = as.Date(Sunset$date)
 
 SunsetStartOnly <- Sunset$sunsetStart
 SunsetStartOnly <- as.POSIXct(SunsetStartOnly)
 
+#Pulling values from Survey Data
+
+Vessel.No <- SurveyData$Vessel.No
+Survey_Start <- as.character(SurveyData$StartTime)
+format <- "%Y-%m-%d %H:%M:%S"
+SurveyStarted <- as.POSIXct(paste(date, Survey_Start), format=format)
+
+Sunset_Time <- as.numeric(SunsetStartOnly, "%H:%M:%S")
+       
+
+
 #Bind Data
-SunsetStart <- full_join(SunsetStart, (data.frame(Survey_Date = date,
-                                                 Year = as.double(Year),
-                                                 sunsetStart = as.character(SunsetStartOnly),
-                                                 Comments = "NA")))
-SunsetStart <- SunsetStart %>% mutate(id = row_number())
+SSB <- full_join(SSB, (data.frame(Survey_Date = date,
+                                  Survey.No = Survey.No,
+                                  Ground = Ground,
+                                  Vessel.No = Vessel.No,
+                                  Counted = Counted,
+                                  Year = as.double(Year),
+                                  Survey_Type = Survey_Type,
+                                  Survey_Start = Survey_Start,
+                                  SurveyStarted = SurveyStarted,
+                                  Sunset_Time = Sunset_Time,
+                                  sunsetStart = as.character(SunsetStartOnly),
+                                  Comments = Comments)))
+SSB <- SSB %>% mutate(id = row_number())
