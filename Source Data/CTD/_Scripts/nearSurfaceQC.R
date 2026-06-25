5b8c8f00d274d8a800a2497e93d67e0b298ab463
HerringScience <jmunden@herringscience.ca>
2026-06-25 11:27:07 -0300

    updated script folder for CTD

---
+++ Source Data/CTD/_Scripts/nearSurfaceQC.R
@@ -0,0 +1,4 @@
+
+
+### Must do a comparison with valid HSC casts in Scots Bay (i.e. no funny business at the surface) between the first measurement of temperature to the 5-10m average - are there significant differences?
+
