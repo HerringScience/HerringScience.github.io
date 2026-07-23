0cdff4d7ff273804284e459689c7d39c37404973
HerringScience <herringsciencetech@outlook.com>
2026-07-23 13:00:35 -0300

    2026-07-23
    Tag Returns

--- Source Data/Tagging/Tag Returns/Full Tag Return.R
+++ Source Data/Tagging/Tag Returns/Full Tag Return.R
@@ -40,18 +40,24 @@ boxes = read.csv("surveyBoxes.csv")
 
 #Previously entered
 tagReturns2023 <-read.csv("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Source Data/Tagging/Tag Returns/Tag Returns spreadsheets/Compiled tag return spreadsheets/2023/2023 Tag Returns.csv")
-tagReturns2024 <-read.csv("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Source Data/Tagging/Tag Returns/Tag Returns spreadsheets/Compiled tag return spreadsheets/2024/2024 Tag Returns.csv")
+tagReturns <-read.csv("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Source Data/Tagging/Tag Returns/Tag Returns spreadsheets/Compiled tag return spreadsheets/2024/2024 Tag Returns.csv")
 tagReturns2025 <-read.csv("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Source Data/Tagging/Tag Returns/Tag Returns spreadsheets/Compiled tag return spreadsheets/2025/2025 Tag Returns.csv")
 complete.returns <- read.csv("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Source Data/Tagging/complete.returns.csv")
 complete.returns <- complete.returns %>%
   dplyr::select(-X)
-tagReturns <- complete.returns
 
 #Year spreadsheets - To be entered
 tagReturns1 <- read.csv("C:/Users/herri/Documents/GitHub/HerringScience.github.io/Source Data/Tagging/Tag Returns/Tag Returns spreadsheets/Compiled tag return spreadsheets/2026/2026 Tag Returns.csv")
   
 tagReturns$Tag_Num = as.numeric(tagReturns$Tag_Num)
-  #Add in missing tag num/incomplete tag number count and sheet
+
+#Remove damaged/missing/incomplete tag numbers and put in their own dataset
+removed_missing_tag <- tagReturns %>%
+  dplyr::filter(is.na(Tag_Num)) %>%
+  dplyr::mutate(RemovalReason = "Missing complete Tag Number")
+
+tagReturns <- tagReturns %>%
+  dplyr::filter(!is.na(Tag_Num))
   
   tagReturns$returnedArea = as.character(tagReturns$returnedArea)
 
@@ -88,146 +94,41 @@ grounds_sf <- st_sf(
 
 #Change coordinates in tagReturns to decimal degrees
 dmm_to_dd <- function(x) {
-  sapply(strsplit(as.character(x), "\\s+"), function(z) {
-    as.numeric(z[1]) + as.numeric(z[2]) / 60
-  })
+  as.numeric(
+    sapply(strsplit(trimws(as.character(x)), "\\s+"), function(z) {
+      as.numeric(z[1]) + as.numeric(z[2]) / 60
+    })
+  )
 }
 
 tagReturns$returnedLat <- dmm_to_dd(tagReturns$returnedLat)
 tagReturns$returnedLon <- -dmm_to_dd(tagReturns$returnedLon)
 
-#Insert lat and lon from timGrounds if Ground is present.
-
-ground_lookup <- timGrounds %>%
-  group_by(Box) %>%
-  summarise(
-    fill_lon = first(X),
-    fill_lat = first(Y),
-    .groups = "drop"
-  )
-
-#Clean up any extra white space from Grounds
-
-tagReturns <- tagReturns %>%
-  mutate(
-    returnedArea = trimws(returnedArea)
-  )
-
-ground_lookup <- ground_lookup %>%
-  mutate(
-    Box = trimws(Box)
-  )
-
-#Complete lat/lon/Ground
-
-tagReturns <- tagReturns %>%
-  mutate(
-    returnedLat = na_if(as.character(returnedLat), ""),
-    returnedLon = na_if(as.character(returnedLon), ""),
-    returnedLat = as.numeric(returnedLat),
-    returnedLon = as.numeric(returnedLon)
-  )
-
-tagReturns <- tagReturns %>%
-  left_join(
-    ground_lookup,
-    by = c("returnedArea" = "Box")
+removed_missing_location <- tagReturns %>%
+  dplyr::filter(
+    (is.na(returnedArea) | returnedArea == "") &
+      (is.na(returnedLat) | is.na(returnedLon))
   ) %>%
-  mutate(
-    returnedLon = coalesce(returnedLon, fill_lon),
-    returnedLat = coalesce(returnedLat, fill_lat)
-  ) %>%
-  select(-fill_lon, -fill_lat)
-
-#Missing lat/lons therefore unable to track tag return
-
-removed_missing_coords <- tagReturns %>%
-  filter(
-    is.na(returnedLat) |
-      is.na(returnedLon) |
-      trimws(returnedLat) == "" |
-      trimws(returnedLon) == ""
-  ) %>%
-  mutate(
-    RemovalReason = "Missing return coordinates"
+  dplyr::mutate(
+    RemovalReason = "Missing return area and latitude/longitude"
   )
+
+# Keep records that have either: A return area AND/OR both latitude and longitude
 
 tagReturns <- tagReturns %>%
-  filter(
-    !is.na(returnedLat),
-    !is.na(returnedLon),
-    trimws(returnedLat) != "",
-    trimws(returnedLon) != ""
-  )
-
-
-events_sf <- st_as_sf(
-  tagReturns,
-  coords = c("returnedLon", "returnedLat"),
-  crs = 4326,
-  remove = FALSE
-)
-
-# Spatial join
-events_joined <- st_join(
-  events_sf,
-  grounds_sf["Box"],
-  join = st_within
-)
-
-# Replace Other with polygon-assigned ground
-events_joined <- events_joined %>%
-  mutate(
-    returnedArea = case_when(
-      returnedArea == "Other" & !is.na(Box) ~ Box,
-      TRUE ~ returnedArea
+  dplyr::filter(
+    !(
+      (is.na(returnedArea) | returnedArea == "") &
+        (is.na(returnedLat) | is.na(returnedLon))
     )
   )
-
-# Remove geometry if desired
-taggingEvents <- events_joined %>%
-  st_drop_geometry()
-
-#Track tags without area
-removed_missing_area <- tagReturns %>%
-  filter(
-    is.na(returnedArea) |
-      trimws(returnedArea) == ""
-  ) %>%
-  mutate(
-    RemovalReason = "Could not assign return area"
-  )
-
-needs_fill <- (
-  is.na(tagReturns$returnedArea) |
-    trimws(tagReturns$returnedArea) == "" |
-    toupper(trimws(tagReturns$returnedArea)) %in%
-    c("UNKNOWN", "UNKNOWN GROUND")
-) &
-  !is.na(tagReturns$returnedLat) &
-  !is.na(tagReturns$returnedLon)
-
-pts <- st_as_sf(
-  tagReturns[needs_fill, ],
-  coords = c("returnedLon", "returnedLat"),
-  crs = 4326
-)
-
-joined <- st_join(
-  pts,
-  grounds_sf["Box"],
-  join = st_intersects,
-  left = TRUE
-)
-
-tagReturns$returnedArea[needs_fill] <- joined$Box
 
 #Add Julien Date
 
 tagReturns <- tagReturns %>%
   mutate(
-    returnedDate = ymd(returnedDate),   # yyyy/mm/dd or dd/mm/yyyy. Seems to keep switching between the two.
-    #returnedDate = dmy(returnedDate),
+    #returnedDate = ymd(returnedDate),   # yyyy/mm/dd or dd/mm/yyyy. Seems to keep switching between the two.
+    returnedDate = dmy(returnedDate),
     returnedJulian = yday(returnedDate)
   )
 
@@ -237,7 +138,7 @@ removed_bad_dates <- tagReturns %>%
   ) %>%
   filter(is.na(returnedDate_parsed)) %>%
   mutate(
-    RemovalReason = "Invalid returnedDate"
+    RemovalReason = "Invalid returned Date"
   )
 
 
@@ -325,8 +226,6 @@ fullReturns <- fullReturns %>%
     daysAtLarge = as.numeric(returnedDate - Date)
   )
 
-#remove any tags that the values are negative. Must go through this list later to figure out what happened.
-
 #Tracking removed tags
 
 removed_negative_days <- fullReturns %>%
@@ -364,10 +263,16 @@ fullReturns <- fullReturns %>%
     Comments
   )
 
-fullReturnsCSV$Ground <- as.character(fullReturnsCSV$Ground)
-fullReturnsCSV$returnedArea <- as.character(fullReturnsCSV$returnedArea)
-fullReturnsCSV$dataorigin <- as.character(fullReturnsCSV$dataorigin)
-fullReturnsCSV$Comments <- as.character(fullReturnsCSV$Comments)
+fullReturnsCSV <- fullReturnsCSV %>%
+  mutate(across(everything(), as.character))
+
+fullReturns <- fullReturns %>%
+  mutate(across(everything(), as.character))
+
+# fullReturnsCSV$Ground <- as.character(fullReturnsCSV$Ground)
+# fullReturnsCSV$returnedArea <- as.character(fullReturnsCSV$returnedArea)
+# fullReturnsCSV$dataorigin <- as.character(fullReturnsCSV$dataorigin)
+# fullReturnsCSV$Comments <- as.character(fullReturnsCSV$Comments)
 
 #Combine to FullReturnsCSV
 fullReturnsCSV <- bind_rows(fullReturnsCSV, fullReturns)
@@ -379,19 +284,34 @@ fullTagReturns <- write_csv(fullReturnsCSV, "Full Returns.csv" )
 
 
 #Write/Update tags that were found but had to be removed for some reason.
-removed_missing_coords <- removed_missing_coords %>%
-  mutate(across(everything(), as.character))
-
 removed_negative_days <- removed_negative_days %>%
   mutate(across(everything(), as.character))
 
-removed_missing_area <- removed_missing_area %>%
+removed_missing_tag <- removed_missing_tag %>%
   mutate(across(everything(), as.character))
 
+removed_missing_location <- removed_missing_location %>%
+  mutate(across(everything(), as.character))
+
+removed_bad_dates <- removed_bad_dates %>%
+  mutate(across(everything(), as.character))
+
+# removed_missing_locations <- removed_missing_locations %>%
+#   mutate(across(everything(), as.character))
+
+#removed_missing_coords <- removed_missing_coords %>%
+#  mutate(across(everything(), as.character))
+
+#removed_missing_area <- removed_missing_area %>%
+#  mutate(across(everything(), as.character))
+
 removed_tags <- bind_rows(
-  removed_missing_coords,
+#  removed_missing_coords,
   removed_negative_days,
-  removed_missing_area
+  removed_missing_tag,
+  removed_missing_location,
+  removed_bad_dates,
+#  removed_missing_area
 ) %>%
   distinct(Tag_Num, .keep_all = TRUE)
 
