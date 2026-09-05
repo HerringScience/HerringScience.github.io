936635b20c2d1d47db104e7d2ce2de745ff63bc3
HerringScience <herringsciencetech@outlook.com>
2026-09-04 20:13:33 -0700

    GB2 Results

--- Update Data.R
+++ Update Data.R
@@ -2,17 +2,17 @@
 rm(list = ls())
 
 # IMPORTANT : SET GROUND, YEAR, AND SURVEY # HERE
-surv="SB" #SB or GB or SI
-surv2="Scots Bay" #"German Bank", "Seal Island" or "Scots Bay" as written
+surv="GB" #SB or GB or SI
+surv2="German Bank" #"German Bank", "Seal Island" or "Scots Bay" as written
 year="2026"
-surv.no="8"
+surv.no="2"
 adhoc = "false" #true or false if an adhoc survey was completed (and "adhoc.csv" exists)
 Sample = "Y" #whether ("Y") or not ("N") they caught fish during this survey window
 Tow = "N" #whether or not plankton tow(s) were conducted
 
 #(SB ONLY) Set main-box vessels
 ## (SB ONLY) OG was main-box vessels only, but then it stopped doing distance properly. Add in all vessels here.
-ids = c("FM", "LJ", "LM", "MS")
+ids = c("BP", "LB", "TM", "FM", "LJ", "LM", "MS")
 
 #Area and TS values - From table C
 SB1= 675.4278 #SB main area
